67c10e664c9c649fbda06f9995204c96c50cf6fe
oeg8168 <oeg8168@gmail.com>
2015-06-17 21:07:59 +0800

    Create class GA_Final_Project_Main for main program

---
+++ src/Main/GA_Final_Project_Main.java
@@ -0,0 +1,15 @@
+package Main;
+/**
+ * 
+ * @author LISM_OEG
+ *
+ */
+public class GA_Final_Project_Main {
+
+	public static void main(String[] args) {
+		
+		
+
+	} // end of main()
+
+} // end of class GA_Final_Project_Main
