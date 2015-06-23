d8a6c1ab6b166fe920b460e32cf2af8f5e71f868
oeg8168 <oeg8168@gmail.com>
2015-06-23 10:39:24 +0800

    Add some global parameters

--- src/Main/GlobalParam.java
+++ src/Main/GlobalParam.java
@@ -1,5 +1,13 @@
 package Main;
 
 public class GlobalParam {
+	public static final int CAR_WIDTH = 30;
+	public static final int CAR_HEIGHT = 65;
+
+	public static final String INPUT_FOLDER_PATH = "./input_img/";
+	public static final String OUTPUT_FOLDER_PATH = "./output/";
+
+	public static final int RECORD_INTERVAL = 1;
+	public static final int MAX_CARS = 100;
 
 } // end of class GlobalParam
